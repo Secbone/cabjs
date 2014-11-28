2155838f24be1024f7bb430509ed74e1f8d229c6
Secbone <secbone@gmail.com>
2014-11-28 22:15:00 +0800

    add

--- dest/main.js
+++ dest/main.js
@@ -1,10 +1,59 @@
 (function() {
-  var Bonejs;
-
-  Bonejs = Bonejs || {};
+  var Bonejs,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   Bonejs = (function() {
-    function Bonejs() {}
+    function Bonejs(selector, size) {
+      this._autoResize = __bind(this._autoResize, this);
+      selector = selector || 'canvas';
+      size = size || [1, 1];
+      this._canvas = document.querySelector(selector);
+      this._parent = this._canvas.parentNode;
+      this._painter = this._canvas.getContext('2d');
+      this.setSize(size);
+      this._bindEvent();
+    }
+
+    Bonejs.prototype.setSize = function(size) {
+      this.setWidth(size[0]);
+      return this.setHeight(size[1]);
+    };
+
+    Bonejs.prototype.setWidth = function(width) {
+      var canvasWidth;
+      this._width = width;
+      canvasWidth = width > 1 ? width : this._canvas.parentNode.offsetWidth * width;
+      return this._canvas.style.width = "" + canvasWidth + "px";
+    };
+
+    Bonejs.prototype.setHeight = function(height) {
+      var canvasHeight;
+      this._height = height;
+      canvasHeight = height > 1 ? height : this._canvas.parentNode.offsetHeight * height;
+      return this._canvas.style.height = "" + canvasHeight + "px";
+    };
+
+    Bonejs.prototype.getWidth = function() {
+      return this._canvas.offsetWidth;
+    };
+
+    Bonejs.prototype.getHeight = function() {
+      return this._canvas.offsetHeight;
+    };
+
+    Bonejs.prototype._autoResize = function() {
+      return this.setSize([this._width, this._height]);
+    };
+
+    Bonejs.prototype._bindEvent = function() {
+      return window.addEventListener('resize', this._autoResize);
+    };
+
+    Bonejs.prototype.fillRect = function(rectArray, color) {
+      var _ref;
+      this._painter.fillStyle = color;
+      return (_ref = this._painter).fillRect.apply(_ref, rectArray);
+    };
 
     return Bonejs;
 
--- src/main.coffee
+++ src/main.coffee
@@ -1,1 +1,31 @@
 class Bonejs
+    constructor: (selector, size) ->
+        selector = selector || 'canvas'
+        size = size || [1, 1]
+        @_canvas = document.querySelector selector
+        @_parent = @_canvas.parentNode
+        @_painter = @_canvas.getContext '2d'
+        @setSize size
+        @_bindEvent()
+    setSize: (size) ->
+        @setWidth size[0]
+        @setHeight size[1]
+    setWidth: (width) ->
+        @_width = width
+        canvasWidth = if width>1 then width else @_canvas.parentNode.offsetWidth*width
+        @_canvas.style.width = "#{canvasWidth}px"
+    setHeight: (height) ->
+        @_height = height
+        canvasHeight = if height>1 then height else @_canvas.parentNode.offsetHeight*height
+        @_canvas.style.height = "#{canvasHeight}px"
+    getWidth: ->
+        @_canvas.offsetWidth
+    getHeight: ->
+        @_canvas.offsetHeight
+    _autoResize: =>
+        @setSize [@_width, @_height]
+    _bindEvent: ->
+        window.addEventListener 'resize', @_autoResize
+    fillRect: (rectArray, color) ->
+        @_painter.fillStyle = color
+        @_painter.fillRect rectArray...
